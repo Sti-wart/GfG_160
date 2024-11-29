c5841cf0a10dfdae62c00457ed82c72ffb592305
STIWART <138205066+Sti-wart@users.noreply.github.com>
2024-11-30 01:56:31 +0530

    Add files via upload

---
+++ ARRAY/Day 3 Array Reverse/using_inbuilt_methods.cpp
@@ -0,0 +1,23 @@
+// C++ Program to reverse an array using inbuilt methods
+
+//Using Inbuilt Methods - O(n) Time and O(1) Space
+
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+using namespace std;
+
+// function to reverse an array
+void reverseArray(vector<int> &arr) {
+    reverse(arr.begin(), arr.end());
+}
+
+int main() {
+    vector<int> arr = { 1, 4, 3, 2, 6, 5 };
+
+    reverseArray(arr);
+  
+    for(int i = 0; i < arr.size(); i++) 
+        cout << arr[i] << " ";
+    return 0;
+}
---
+++ ARRAY/Day 3 Array Reverse/using_recursion.cpp
@@ -0,0 +1,35 @@
+// C++ Program to reverse an array using Recursion
+
+//[Alternate Approach] Using Recursion - O(n) Time and O(n) Space
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// recursive function to reverse an array from l to r
+void reverseArrayRec(vector<int> &arr, int l, int r) {
+    if(l >= r)
+        return;
+  
+    // Swap the elements at the ends
+    swap(arr[l], arr[r]);
+  
+    // Recur for the remaining array
+    reverseArrayRec(arr, l + 1, r - 1);
+}
+
+// function to reverse an array
+void reverseArray(vector<int> &arr) {
+    int n = arr.size();
+    reverseArrayRec(arr, 0, n - 1);
+}
+
+int main() {
+    vector<int> arr = { 1, 4, 3, 2, 6, 5 };
+
+    reverseArray(arr);
+  
+    for(int i = 0; i < arr.size(); i++) 
+        cout << arr[i] << " ";
+    return 0;
+}
